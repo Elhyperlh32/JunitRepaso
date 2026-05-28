9f4ec1f5c7cdf327f75239b9115ee8f3b3263c6e
Elhyperlh32 <dylanaaguilar07@gmail.com>
2026-05-28 12:23:43 +0200

    Create s.java

---
+++ TareasRepasoJUnit/src/s.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class s {
+    static void main() {
+        Alreves()
+    }
+    public static int[] Alreves(int[]v){
+        List<int []> reves = List.of(v);
+        reves.reversed();
+        int[] newV = new int[reves.size()];
+        return newV;
+    }
+}
